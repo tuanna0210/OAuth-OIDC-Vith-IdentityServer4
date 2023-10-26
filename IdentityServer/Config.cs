465fc0b7080fe3326785e70843d0236c956712e6
Tuan Nguyen <anhtuann0210@gmail.com>
2023-10-26 22:43:38 +0700

    config test user

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,10 +1,30 @@
 ﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System.Collections.Generic;
 
 namespace IdentityServer
 {
     public static class Config
     {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+        }
+
         public static IEnumerable<ApiResource> GetApiResources()
         {
             return new List<ApiResource>
@@ -12,13 +32,20 @@ namespace IdentityServer
                 new ApiResource("api1", "My API")
             };
         }
+        public static IEnumerable<ApiScope> ApiScopes =>
+        new[]
+        {
+            new ApiScope("api1"),
+  
+        };
+
         public static IEnumerable<Client> GetClients()
         {
             return new List<Client>
             {
                 new Client
                 {
-                    ClientId = "client",
+                    ClientId = "console-client",
 
                     // no interactive user, use the clientid/secret for authentication
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
@@ -26,7 +53,7 @@ namespace IdentityServer
                     // secret for authentication
                     ClientSecrets =
                     {
-                        new Secret("secret".Sha256())
+                        new Secret("console-client-secret".Sha256())
                     },
 
                     // scopes that client has access to
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -16,6 +16,12 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()//T-TEMP
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddTestUsers(Config.GetUsers()); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +31,7 @@ namespace IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseIdentityServer();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
