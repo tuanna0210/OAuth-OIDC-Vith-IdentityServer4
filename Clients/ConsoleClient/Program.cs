e101509a0b1778be41b652c5c1cbed486bd0c44d
Tuan Nguyen <anhtuann0210@gmail.com>
2023-10-26 22:40:27 +0700

    initial console client

---
+++ Clients/ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel.Client;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ConsoleClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var _discoveryDocument = await client.GetDiscoveryDocumentAsync("https://localhost:5551");
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = _discoveryDocument.TokenEndpoint,
+
+                ClientId = "console-client",
+                ClientSecret = "console-client-secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
